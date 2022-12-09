f91145b3dff28a5c317ad23c98092eb700da2237
Casimir Josias <88723085+CasimirJosias@users.noreply.github.com>
2022-12-08 21:31:13 -0500

    third commit

---
+++ enonse10.js
@@ -0,0 +1,25 @@
+const bouton=document.querySelector('#copy');
+const h1=document.querySelector('h1');
+const p=document.querySelector('p');
+
+h1.addEventListener('selectstart',(e)=>{
+    e.preventDefault();
+});
+p.addEventListener('selectstart',(e)=>{
+    e.preventDefault();
+});
+h1.addEventListener('copy',(e)=>{
+    e.preventDefault();
+});
+p.addEventListener('copy',(e)=>{
+    e.preventDefault();
+});
+bouton.onclick=()=>{
+   let range=new Range();
+  range.setStart(h1.firstChild, 0) ;
+  range.setEnd(p.firstChild, p.textContent.length);
+  let seleksyon=document.getSelection();
+  seleksyon.removeAllRanges();
+  seleksyon.addRange(range);
+  navigator.clipboard.writeText(seleksyon.toString());
+}
